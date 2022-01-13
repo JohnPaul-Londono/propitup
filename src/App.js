6f17b80613dd2114f35e7895e2acb711502a2496
JohnPaul-Londono <johnplondono@gmail.com>
2022-01-13 17:54:38 -0600

    Added Functional Component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import './App.css';
 import PersonCard from './components/PersonCard';
+import Functional from './components/functional';
 
 
 function App() {
   return (
     <div className="App">
       <p>Prop It Up</p>
-      <PersonCard lastName = "London , " firstName = "Kya" age = {2} hairColor = "Hair Color: White"/>
+      {/* <PersonCard lastName = "London , " firstName = "Kya" age = {2} hairColor = "Hair Color: White"/>
       <PersonCard lastName = "Patterson , " firstName = "Bumi" age = {1} hairColor = "Hair Color: Orange"/>
       <PersonCard lastName = "London , " firstName = "Jon" age = {29} hairColor = "Hair Color: Black"/>
-      <PersonCard lastName = "Patterson , " firstName = "Stephen" age = {30} hairColor = "Hair Color: Brown"/>
+      <PersonCard lastName = "Patterson , " firstName = "Stephen" age = {30} hairColor = "Hair Color: Brown"/> */}
+      <Functional/>
     </div>
   );
 }
---
+++ src/components/functional.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+
+const Functional = (props) => {
+    
+    
+
+    return (
+        <div>
+            <h1>London, Kya</h1>
+            <div>Age: 2</div>
+            <div>Hair Color: White</div>
+            <h1>Patterson, Bumi</h1>
+            <div>Age: 1</div>
+            <div>Hair Color: Orange</div>
+        </div>
+    )
+}
+
+export default Functional;
